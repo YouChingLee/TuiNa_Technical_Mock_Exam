248c5103aa2f0080bcab0901e2bc57b919504530
YouChingLee <fcrdxesz@hotmail.com>
2021-12-17 13:38:21 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1d4fa1d0554afa414256b48c24192911",
-"/": "1d4fa1d0554afa414256b48c24192911",
+"index.html": "6860b43bbc6ce381918b4d0db9da3098",
+"/": "6860b43bbc6ce381918b4d0db9da3098",
 "main.dart.js": "f3d5ffc1740df590de9dc641e410ec95",
 "manifest.json": "79883986620cd55920872b34202313c6",
 "version.json": "c995bd67fc1e4b803c1f811de17ef7e4"
